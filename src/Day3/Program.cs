4eaf5faf306016ea84c788478643fda735965fda
Filipe Toscano <filipe.toscano@gmail.com>
2025-02-01 12:25:21 +0400

    Day 3: Part 1

---
+++ src/Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+var fname = args.Count() > 0 ? args[ 0 ] : "in.txt";
+var input = File.ReadAllText( fname );
+
+
+var regex = new Regex( @"mul\((?<lv>\d{1,3}),(?<rv>\d{1,3})\)" );
+
+var mc = regex.Matches( input );
+var muls = new List<int>();
+
+foreach ( Match m in mc )
+{
+    var lv = int.Parse( m.Groups[ "lv" ].Value );
+    var rv = int.Parse( m.Groups[ "rv" ].Value );
+
+    var mul = lv * rv;
+
+    muls.Add( mul );
+}
+
+
+var sum = muls.Sum();
+
+Console.WriteLine( "M={0}", string.Join( " ", muls ) );
+Console.WriteLine( "MS={0}", sum );
