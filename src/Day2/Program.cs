9641fcb6e48561ae90227830580f9e11e3f67d50
Filipe Toscano <filipe.toscano@gmail.com>
2025-02-01 12:06:54 +0400

    Day 2: Round 2

--- src/Day2/Program.cs
+++ src/Day2/Program.cs
@@ -10,18 +10,14 @@ foreach ( var report in input.Split( "\n" ) )
 
     var r = report.Split( " ", StringSplitOptions.RemoveEmptyEntries ).Select( x => int.Parse( x ) ).ToList();
 
-    var l1 = r[ 0 ];
-    var l2 = r[ 1 ];
+    var l1 = r.First();
+    var ln = r.Last();
 
-    if ( l1 == l2 )
-        continue;
+    var min = ( l1 > ln ) ? -3 : 1;
+    var max = ( l1 > ln ) ? -1 : 3;
 
-    var min = ( l1 > l2 ) ? -3 : 1;
-    var max = ( l1 > l2 ) ? -1 : 3;
-
-    if ( IsSafe( r, min, max ) == true )
-    {
-        Console.WriteLine( "safe: {0}", report );
+    if ( IsSafe( r, min, max, 1 ) == true )
+    { 
         safe++;
     }
     else
@@ -36,21 +32,45 @@ Console.WriteLine( "Nr Safe={0}", safe );
 /*
  * 
  */
-bool IsSafe( List<int> r, int min, int max )
+bool IsSafe( List<int> r, int min, int max, int tolerance )
 {
     for ( int i = 0; i < r.Count - 1; i++ )
     {
         var d = r[ i + 1 ] - r[ i ];
 
-        if ( d == 0 )
-            return false;
+        if ( d == 0 || d < min || d > max )
+        {
+            if ( tolerance == 0 )
+                return false;
 
-        if ( d < min )
-            return false;
+            // Left
+            var r1 = CopyWithout( r, i );
+            var is1 = IsSafe( r1, min, max, tolerance - 1 );
 
-        if ( d > max )
+            if ( is1 == true )
+                return true;
+
+            // Right
+            var r2 = CopyWithout( r, i + 1 );
+            var is2 = IsSafe( r2, min, max, tolerance - 1 );
+
+            if ( is2 == true )
+                return true;
+
             return false;
+        }
     }
 
+    Console.WriteLine( "safe {0}: {1}", tolerance, string.Join( " ", r ) );
     return true;
+}
+
+
+List<int> CopyWithout( List<int> r, int ix )
+{
+    var rd = new List<int>();
+    rd.AddRange( r );
+    rd.RemoveAt( ix );
+
+    return rd;
 }
